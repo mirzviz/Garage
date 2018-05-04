e50c5f31180baba15a2a4d43f5e7f242a0a13e7e
Zvi Lychee <37471473+mirzviz@users.noreply.github.com>
2018-05-04 18:22:10 +0300

    Add files via upload

---
+++ B18 Ex03 InbalSchkolnik 205786148 ZviMirimsky 203121967/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex03.GarageLogic;
+
+namespace Ex03.ConsoleUI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Garage gr = new Garage();
+            gr.addVehicle(new CombustionMotorcycle(new MotorcycleProperties(3, LicenseType.A1), "WheelMan", "ModelPoopi", "1234"));
+            gr.addVehicle(new ElectricMotorcycle(new MotorcycleProperties(42, LicenseType.B2), "electoWheelMan", "ModelElectrico", "021"));
+            gr.addVehicle(new CombustionCar(new CarProperties(Color.Gray, 4), "BebeCarWheel", "combusCar", "000"));
+
+            Console.WriteLine(gr.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
